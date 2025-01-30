ef493efc4f1ff8440610fe79840cdb10f779175f
Abel Rovira Álvarez <165647659+wh4tisnet@users.noreply.github.com>
2025-01-30 14:46:11 +0100

    Delete Batch User Creation Script in PowerShell.ps1

--- Batch User Creation Script in PowerShell.ps1
+++
@@ -1,7 +0,0 @@
-$datos = Get-Content C:\Users\user\Desktop\users.txt
-
-$pass = Read-Host "Introduce una contraseña para los usuarios" -AsSecureString
-
-foreach ($i in $datos){
-    New-LocalUser $i -Password $pass
-}
